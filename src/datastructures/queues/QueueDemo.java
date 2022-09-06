3cb6e352b9c3a146ae7381e62f7d03cb2b91a4a7
Souradeep Das <souradeep.das1995@gmail.com>
2022-09-06 17:25:08 +0530

    Stacks and Queues data structures in java (updated)

--- src/datastructures/queues/QueueDemo.java
+++ src/datastructures/queues/QueueDemo.java
@@ -8,16 +8,24 @@ import java.util.Queue;
 * offer() - add elements to the tail of the queue
 * poll() - remove elements from the head of the queue
 * peek() - view element at the head of the queue
+* isEmpty() - checks if queue is empty
+* size() - returns size of queue
+* contains(E) - returns true if object E is present in queue else returns false
 * */
 public class QueueDemo {
     public static void main(String[] args) {
         Queue<String> q = new LinkedList<>();
+        System.out.println(q.isEmpty());    // true
+        System.out.println(q.size());       // 0
+
         q.offer("Ram");
         q.offer("Shyam");
         q.offer("Jadhu");
         q.offer("Madhu");
 
         System.out.println(q);      // [Ram, Shyam, Jadhu, Madhu]
+        System.out.println(q.isEmpty());    // false
+        System.out.println(q.size());       // 4
 
         String polledObject = q.poll();     // removing an object from head of queue
         System.out.println(polledObject);   // Ram
@@ -30,3 +38,11 @@ public class QueueDemo {
         System.out.println(queueHead);       // Jadhu
     }
 }
+
+
+/*
+ * Uses of Queues:
+ * 1. used in buffers (keyboard buffers)
+ * 2. job queues (printer jobs)
+ * 3. used linked lists, priority queues, BFS
+ * */
--- src/datastructures/stacks/StacksDemo.java
+++ src/datastructures/stacks/StacksDemo.java
@@ -8,12 +8,16 @@ import java.util.Stack;
 * push() - insert at the top
 * pop() - remove from top
 * peek() - view top
+* empty() / isEmpty() - checks if queue is empty
+* size() - returns size of queue
+* search(E) - returns 1-based index for search object E if present, else -1 if not present
 * */
 public class StacksDemo {
     public static void main(String[] args) {
         Stack<String> st = new Stack<>();
 
         System.out.println(st.empty());     // true
+        System.out.println("abc" +  st.isEmpty());
 
         st.push("Apple");
         st.push("Orange");
@@ -37,7 +41,7 @@ public class StacksDemo {
 }
 
 /*
-* Uses of stacks:
+* Uses of Stacks:
 * 1. undo/redo features in text editors
 * 2. forward/backward in browsers
 * 3. used in backtracking (mazes, file directories)
